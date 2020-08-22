f9846fe3caba44f2fcaf2a7a62c0f84b91860290
Mouhamed Marwan <61049928+Mistydz@users.noreply.github.com>
2020-08-22 22:33:53 +0100

    Update fetch.js

--- public/fetch.js
+++ public/fetch.js
@@ -1,12 +1,7 @@
-// Welcome to my ghetto Javascript codding feel free to use any $hit in here even though ain't much anyway
-// Have a great day
-
+// Project created by misty :) i hope u have a great day.
 
 let user;
 let level;
-
-
-
 
 async function getData() {
     var user = document.getElementById('user').value;
@@ -15,12 +10,15 @@ async function getData() {
     const db_data = await db_response.json();
     window.stop();
 
+    // timestamp to date 
     const { timecreated: time = 907794930 } = db_data;
     const date = new Date(time * 1000);
     const year = date.getFullYear();
     const month = date.toLocaleString('default', { month: 'long' });
     const dt = date.getDate();
     const fullDate = dt + ' ' + month + ' ' + year;
+
+    // user online/offline state with font color green/red
     const { personastate: state = 0 } = db_data;
     if (state == 0) {
         const userstate = 'Offline';
@@ -33,21 +31,18 @@ async function getData() {
 
     }
 
-
-
-
     const { avatarfull: avatarfull = './default_avatar.jpg' } = db_data;
     document.getElementById('img').src = avatarfull;
     document.getElementById('img').style.opacity = "1";
-    const { flag: flag = 'https://restcountries.eu/data/mac.svg' } = db_data;
-    document.getElementById('img2').src = flag;
-    document.getElementById('img2').style.opacity = "1";
     const { personaname: personaname = 'no Name' } = db_data;
     document.getElementById('name1').textContent = 'Name : ';
     document.getElementById('name2').textContent = personaname;
     document.getElementById('country1').textContent = 'Country : ';
-    const { name: country = 'unkown' } = db_data;
+    const { name: country = 'unkown' } = db_data; // if the account has no set country output unkown with a green flag
     document.getElementById('country2').textContent = country;
+    const { flag: flag = 'https://restcountries.eu/data/mac.svg' } = db_data; // green flag
+    document.getElementById('img2').src = flag;
+    document.getElementById('img2').style.opacity = "1";
     document.getElementById('date1').textContent = 'Creation Date : ';
     document.getElementById('date2').textContent = fullDate;
     document.getElementById('date1').textContent = 'Creation Date : ';
@@ -59,8 +54,6 @@ async function getData() {
     document.getElementById('title').style.paddingTop = "0";
 
 
-
-
     const { player_level: pl = 0 } = db_data;
     const { player_xp: xp = 0 } = db_data;
     const diffrencelvl = level - pl;
@@ -68,30 +61,27 @@ async function getData() {
         level = pl + 1
         const calc_xp = (Math.ceil(((level * ((level * 5) + 50)) / 100))) * 100;
         const diffrence = calc_xp - xp;
-        const badges = Math.ceil((diffrence / 100)); // coupons or promo card recived after crafting = sets used
-        const tf2_keys = badges / 20; // const emote = badges * 2; when u craft a badge u get 1 emote 1 background 1 coupon/promoCard
+        const badges = Math.ceil((diffrence / 100)); // sets nedded to craft to get to wanted level
+        const tf2_keys = badges / 20; // u can buy 20 sets for 1 tf2 keys from traiding bot's
 
         document.getElementById('dreamlevel2').textContent = level;
         document.getElementById('xpned2').textContent = diffrence;
         document.getElementById('set2').textContent = badges;
         document.getElementById('tf22').textContent = tf2_keys;
-    } else if (diffrencelvl < 0 || level > 5099) {
+    } else if (diffrencelvl < 0 || level > 5099) { // steam max level that you can get is 5099 (output 0) and diffrencelvl<0 means that the wanted level is smaller then the level that you have now
         document.getElementById('xpned2').textContent = 0;
         document.getElementById('set2').textContent = 0;
         document.getElementById('tf22').textContent = 0;
     } else {
         const calc_xp = (Math.ceil(((level * ((level * 5) + 50)) / 100))) * 100;
         const diffrence = calc_xp - xp;
-        const badges = Math.ceil((diffrence / 100)); // coupons or promo card recived after crafting = sets used
-        const tf2_keys = badges / 20; // const emote = badges * 2; when u craft a badge u get 1 emote 1 background 1 coupon/promoCard
-
+        const badges = Math.ceil((diffrence / 100));
+        const tf2_keys = badges / 20;
         document.getElementById('xpned2').textContent = diffrence;
         document.getElementById('set2').textContent = badges;
         document.getElementById('tf22').textContent = tf2_keys;
-
-
-
     }
+
     document.getElementById('level2').textContent = pl;
     document.getElementById('dreamlevel2').textContent = level;
     document.getElementById('xpnow2').textContent = xp;
