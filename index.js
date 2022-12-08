5f983ba1324df4d2e120028cf6f0d1e2ef73f87d
Mohammed Marwan <61049928+Mistydz@users.noreply.github.com>
2022-12-08 12:45:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.listen(port, () => {
 app.use(express.static('client/build'));
 app.use(cors());
 
-const Steam_API_Key = process.env.API_KEY; // create .env file with Steam_API_Key=XXXXXXXXX in it
+const Steam_API_Key = process.env.API_KEY; // create .env file with API_Key=XXXXXXXXX in it
 
 if (!Steam_API_Key) {
     throw new Error('You need a steam api key to get a response')
@@ -22,103 +22,84 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.get("/id", urlencodedParser, async(req, res) => {
 
 
-    try {
-        const id = req.query.user; // user input
-        if (!id) throw new Error("The username field is empty");
 
-        if ((id * 1) == id) {
-            const steamid = id;
-            const BadgesURL = `http://api.steampowered.com/IPlayerService/GetBadges/v1/?key=${Steam_API_Key}&steamid=${steamid}`;
-            const { response: Badges } = await (await fetch(BadgesURL)).json();
-            if (!Badges) throw new Error("This user info are privet or inexistant , please try again later");
-
-            const { player_xp, player_level, player_xp_needed_to_level_up } = Badges;
-
-            const PlayerSummariesURL = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${Steam_API_Key}&steamids=${steamid}`;
-            const { response: PlayerSummaries } = await (
-                await fetch(PlayerSummariesURL)
-            ).json();
-
-            const {
-                personastate,
-                personaname,
-                profileurl,
-                avatarfull,
-                timecreated,
-                loccountrycode,
-            } = PlayerSummaries.players[0];
-
-
-            const flag = await (await fetch(`https://www.countryflags.io/${loccountrycode}/flat/64.png`)).url;
-            const name = loccountrycode
-
-            const data = {
-                steamid,
-                player_xp,
-                player_level,
-                player_xp_needed_to_level_up,
-                personastate,
-                personaname,
-                profileurl,
-                avatarfull,
-                timecreated,
-                loccountrycode,
-                flag,
-                name,
-            };
-
-            console.log(data)
-            res.json(data);
-        } else {
-            const SteamIdUrl = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${Steam_API_Key}&vanityurl=${id}`;
-            const { response: { success, steamid }, } = await (await fetch(SteamIdUrl)).json();
-            if (success != 1) throw new Error("This user can't be found , please verify your input again");
-
-
-            const BadgesURL = `http://api.steampowered.com/IPlayerService/GetBadges/v1/?key=${Steam_API_Key}&steamid=${steamid}`;
-            const { response: Badges } = await (await fetch(BadgesURL)).json();
-            if (!Badges) throw new Error("This user info are privet or inexistant , please try again later");
-
-            const { player_xp, player_level, player_xp_needed_to_level_up } = Badges;
-
-            const PlayerSummariesURL = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${Steam_API_Key}&steamids=${steamid}`;
-            const { response: PlayerSummaries } = await (
-                await fetch(PlayerSummariesURL)
-            ).json();
-
-            const {
-                personastate,
-                personaname,
-                profileurl,
-                avatarfull,
-                timecreated,
-                loccountrycode,
-            } = PlayerSummaries.players[0];
-
-
-            const flag = await (await fetch(`https://www.countryflags.io/${loccountrycode}/flat/64.png`)).url;
-            const name = loccountrycode
-
-            const data = {
-                steamid,
-                player_xp,
-                player_level,
-                player_xp_needed_to_level_up,
-                personastate,
-                personaname,
-                profileurl,
-                avatarfull,
-                timecreated,
-                loccountrycode,
-                flag,
-                name,
-            };
-
-            console.log(data)
-            res.json(data);
-        }
-
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+    async function getId() {
+    const id = req.query.user;
+    if((id * 1) == id) {
+        var steamid = id;
+        var steam_id=steamid
+    }else{
+        const SteamIdUrl = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${Steam_API_Key}&vanityurl=${id}`;
+        const { response: { success, steamid }, } = await (await fetch(SteamIdUrl)).json();
+        if (success != 1) throw new Error("This user can't be found , please verify your input again");
+        var steam_id=steamid
     }
+    return gotID(steam_id)
+    }
+    getId()
+
+    async function gotID(steam_id){
+        const BadgesURL = `http://api.steampowered.com/IPlayerService/GetBadges/v1/?key=${Steam_API_Key}&steamid=${steam_id}`;
+            const { response: Badges } = await (await fetch(BadgesURL)).json();
+            if (!Badges) throw new Error("This user info are privet or inexistant , please try again later");
+
+            const { player_xp, player_level, player_xp_needed_to_level_up } = Badges;
+
+            const VacURL = `http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=${Steam_API_Key}&steamids=${steam_id}`;
+            const { players: VacStatus } = await (await fetch(VacURL)).json();
+
+                const {
+                    CommunityBanned,
+                    VACBanned,
+                    NumberOfVACBans,
+                    DaysSinceLastBan,
+                    NumberOfGameBans,
+                    EconomyBan,
+                } = VacStatus[0];
+
+            if (!VacStatus) throw new Error("This user info are privet or inexistant , please try again later");
+
+
+
+            const PlayerSummariesURL = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${Steam_API_Key}&steamids=${steam_id}`;
+            const { response: PlayerSummaries } = await (await fetch(PlayerSummariesURL)).json();
+            if (!PlayerSummaries) throw new Error("This user info are privet or inexistant , please try again later");
+
+            const {
+                personastate,
+                personaname,
+                profileurl,
+                avatarfull,
+                timecreated,
+                loccountrycode,
+            } = PlayerSummaries.players[0];
+
+
+
+            const data = {
+                steam_id,
+                player_xp,
+                player_level,
+                player_xp_needed_to_level_up,
+                personastate,
+                personaname,
+                profileurl,
+                avatarfull,
+                timecreated,
+                loccountrycode,
+                CommunityBanned,
+                VACBanned,
+                NumberOfVACBans,
+                DaysSinceLastBan,
+                NumberOfGameBans,
+                EconomyBan,
+            };
+            console.log(data);
+            res.json(data);
+    }
+
+
+
+
+ 
 });
